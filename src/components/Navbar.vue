27c6a206d1c3546e369532d1c1cea7eb950b6db3
satrioprbw <satrioprabowo47@gmail.com>
2023-04-04 20:15:02 +0700

    chore: add new route , register

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,8 @@
 export default {
   data() {
     return {
-      searchInput: ''
+      searchInput: '',
+      token : localStorage.access_token
     }
   }
 }
@@ -13,11 +14,14 @@ export default {
   <div class="card" style="border-radius: 1rem;">
     <div class="row g-0">
       <nav class="navbar fixed-top navbar-expand-lg" style="background-color: #E8DFCA;">
-        <a class="navbar-brand" href="#" style="margin-left: 20px;">
+        <a @click.prevent="$router.push('/')" class="navbar-brand" href="#" style="margin-left: 20px;">
           <img src="../assets/logo-no-background.png" width="180" height="40" alt="logo">
         </a>
-        <div>
-          <button class="btn btn-outline-secondary col-12 me-3">Bookmark</button>
+        <div class="mx-3">
+          <button @click.prevent="$router.push('/bookmark')" class="btn btn-outline-secondary col-12 me-3">Bookmark</button>
+        </div>
+        <div v-if="!token">
+          <button @click.prevent="$router.push('/login')" class="btn btn-outline-secondary col-12 me-3">Login</button>
         </div>
       </nav>
     </div>
